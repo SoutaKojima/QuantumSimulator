f54d94d888c82c3adeca69ab1b3eec6654234ba8
chemical <s.kojima.8263.allez@gmail.com>
2022-10-16 00:03:53 +0900

    Import XCharts and TextMeshPro

--- Assets/Scripts/Quantum.cs
+++ Assets/Scripts/Quantum.cs
@@ -8,23 +8,32 @@ public class Quantum : MonoBehaviour
 {
     [SerializeField]private float _alpha;
     private float _beta;
+    private float _speed;
     void Awake()
     {
         _alpha = Random.Range(0f, 1f);
         _beta = 1 - _alpha;
+        _speed = 3f;
     }
     
     void FixedUpdate()
     {
-        transform.Rotate(new Vector3(0f, 1f, 0f) * Time.deltaTime);
-        transform.position += new Vector3(1f, 0f, 0f) * Time.deltaTime;
+        transform.Rotate(new Vector3(0f, 1f, 0f) * _speed * Time.deltaTime);
+        transform.position += new Vector3(1f, 0f, 0f) * _speed * Time.deltaTime;
     }
 
-    private void OnCollisionExit(Collision other)
+    private void OnTriggerEnter(Collider other)
     {
         if (other.transform.CompareTag("Observer"))
         {
-            
+            if (_alpha > 0.5f)
+            {
+                transform.Translate(new Vector3(0f, 2f, 0f));
+            }
+            else
+            {
+                transform.Translate(new Vector3(0f, -2f, 0f));
+            }
         }
     }
 }
